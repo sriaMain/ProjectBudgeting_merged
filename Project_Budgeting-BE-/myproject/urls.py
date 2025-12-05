c0ec579f5d95ac0107239b29a18b6bb5b28b1031
sriaMain <sriahrdesk@gmail.com>
2025-12-05 12:42:44 +0530

    index.html changed

--- Project_Budgeting-BE-/myproject/urls.py
+++ Project_Budgeting-BE-/myproject/urls.py
@@ -21,21 +21,42 @@ from .views import frontend
 from django.conf import settings
 from django.conf.urls.static import static
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/accounts/', include('accounts.urls')),
+#     path('api/', include('module.urls')),
+#     path('api/', include('product_group.urls')),
+#     path('api/roles/', include('roles.urls')),
+#     path('api/', include('client.urls')),
+#     path("", frontend),
+#     # for any route that is not an API route.
+#     # re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+# ]
+# # FRONTEND ROUTES (React SPA)
+# # Serve index.html for ALL non-API routes
+# urlpatterns += [
+#     re_path(r"^(?!api/).*", TemplateView.as_view(template_name="index.html")),
+# ]
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # API routes
     path('api/accounts/', include('accounts.urls')),
     path('api/', include('module.urls')),
     path('api/', include('product_group.urls')),
     path('api/roles/', include('roles.urls')),
     path('api/', include('client.urls')),
-    path("", frontend),
-    # for any route that is not an API route.
-    # re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
 ]
-# FRONTEND ROUTES (React SPA)
-# Serve index.html for ALL non-API routes
+
+# React SPA fallback route — must be last
 urlpatterns += [
-    re_path(r"^(?!api/).*", TemplateView.as_view(template_name="index.html")),
+    re_path(r'^(?!api/).*$', frontend),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Project_Budgeting-BE-/myproject/views.py
+++ Project_Budgeting-BE-/myproject/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.http import FileResponse
+import os
+from django.conf import settings
 
 def frontend(request):
-    return render(request, "index.html")
+    index_path = os.path.join(
+        settings.BASE_DIR.parent, 
+        "Project_Budgeting-FE-", 
+        "dist", 
+        "index.html"
+    )
+    return FileResponse(open(index_path, "rb"))
