e1cf0b0233249946307a81f33183ad73fa160876
sriaMain <sriahrdesk@gmail.com>
2025-12-05 12:13:39 +0530

    whitenoise error

--- Project_Budgeting-BE-/myproject/settings.py
+++ Project_Budgeting-BE-/myproject/settings.py
@@ -69,7 +69,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware', # This should be after SecurityMiddleware
+    'whitenoise.middleware.WhiteNoiseMiddleware', # This should be after SecurityMiddleware
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -77,7 +77,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'myproject.urls'
